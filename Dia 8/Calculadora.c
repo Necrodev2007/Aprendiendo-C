2716bc412b20a04711594e4059b7f41a795fdb8f
Necrodev2007 <luis.p201403@gmail.com>
2025-10-26 22:46:20 -0400

    practicas

--- Dia 8/Calculadora.c
+++ Dia 8/Calculadora.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 int main()
 {
 float n1,n2,result;
 char op;
+int op_v = 0;
 printf("\n=======Calculadora=======\n\n");
 printf("Ingrese primer numero: ");
-scanf("%f", &n1);
+while(scanf("%f", &n1) != 1){
+printf("Eso no es un numero, ingrese un numero valido: ");
 while ((getchar()) != '\n');
-printf("Ingrese el operador (+,-,*,/): ");
-scanf("%c", &op);
-printf("Ingrese el segundo numero: ");
-scanf("%f", &n2);
+}
+while((getchar()) != '\n');
+system("cls");
 
+do {
+        printf("Ingrese el operador (+,-,*,/): ");
+        scanf(" %c", &op);
+        while((getchar()) != '\n');
+        
+        // Verificar si el operador es válido
+        if (op == '+' || op == '-' || op == '*' || op == '/') {
+            op_v = 1;  // Es válido
+        } else {
+            printf("Operador invalido '%c'! Usa +, -, *, o /\n", op);
+            system("pause");  // Pausa para que vea el error
+            system("cls");
+        }
+    } while (!op_v);  // Repetir hasta que sea válido
+    
+system("cls");
+printf("\nIngrese el segundo numero: ");
+while(scanf("%f", &n2) != 1){
+printf("Eso no es un numero, ingrese un numero valido: ");
+while ((getchar()) != '\n');
+}
+system("cls");
 switch(op){
 
 case '+':
@@ -24,9 +49,13 @@ case '*':
 result = n1 * n2;
 break;
 case '/':
-result = n1 / n2;
-break;
+if(n2 == 0){
+    printf("Error, no puedes dividir entre 0");
+ return 1;
 }
-printf("Resultado de la operacion: %.2f", result);
+result = n1 / n2;
+break;  
+}
+printf("Resultado de la operacion: %.2f\n", result);
 return 0;
 }
